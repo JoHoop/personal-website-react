b9c51a5b989a2bf75117a308dda2c7e508f9ef18
Jo Lienhoop <joli@uni-bremen.de>
2020-07-24 20:46:24 +0100

    Add main text and footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
 import React from "react";
 import "./App.css";
-import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Copyright from "./components/Copyright";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+    },
+    main: {
+        marginTop: theme.spacing(8),
+        marginBottom: theme.spacing(2),
+    },
+    footer: {
+        padding: theme.spacing(3, 2),
+        marginTop: "auto",
+        backgroundColor:
+            theme.palette.type === "light"
+                ? theme.palette.grey[200]
+                : theme.palette.grey[800],
+    },
+}));
+
+export default function App() {
+    const classes = useStyles();
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <Button variant="contained" color="primary">
-                    Hello World
-                </Button>
-            </header>
+        <div className={classes.root}>
+            <CssBaseline />
+            <Container component="main" className={classes.main} maxWidth="sm">
+                <Typography variant="h2" component="h1" gutterBottom>
+                    Sticky footer
+                </Typography>
+                <Typography variant="h5" component="h2" gutterBottom>
+                    {"Pin a footer to the bottom of the viewport."}
+                    {
+                        "The footer will move as the main element of the page grows."
+                    }
+                </Typography>
+                <Typography variant="body1">
+                    Sticky footer placeholder.
+                </Typography>
+            </Container>
+            <footer className={classes.footer}>
+                <Container maxWidth="sm">
+                    <Typography variant="body1">
+                        My sticky footer can be found here.
+                    </Typography>
+                    <Copyright />
+                </Container>
+            </footer>
         </div>
     );
 }
-
-export default App;
