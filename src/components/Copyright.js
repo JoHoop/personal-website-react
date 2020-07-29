c8930657b7b986efc25f54d4f851e69ac5dc583a
Jo <jolienhoop@gmail.com>
2020-07-29 14:10:29 +0100

    Add copyright to footer

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,20 +1,11 @@
 import React from "react";
 import { Typography, Link } from "@material-ui/core";
-import Resume from "../resume.json";
 
 export const Copyright = () => {
     return (
         <Typography variant="body2" color="textSecondary">
-            <i className="fas fa-code" /> at <i className="fas fa-moon" /> by{" "}
-            <Link
-                color="inherit"
-                underline="none"
-                href="https://github.com/JoHoop"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <strong>{Resume.basics.name}</strong>
-            </Link>
+            {"Copyright ©"} {new Date().getFullYear()}
+            {"."}
         </Typography>
     );
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { Container } from "@material-ui/core";
+import { Container, Typography, Link } from "@material-ui/core";
 import { Copyright } from "./Copyright";
+import Resume from "../resume.json";
 
 export const Footer = (props) => {
     return (
         <footer className={props.footerClasses}>
             <Container maxWidth="sm">
+                <Typography variant="body1" color="textSecondary">
+                    <i className="fas fa-code" /> at{" "}
+                    <i className="fas fa-moon" /> by{" "}
+                    <Link
+                        color="inherit"
+                        underline="none"
+                        href="https://github.com/JoHoop"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <strong>{Resume.basics.name}</strong>
+                    </Link>
+                </Typography>
                 <Copyright />
             </Container>
         </footer>
