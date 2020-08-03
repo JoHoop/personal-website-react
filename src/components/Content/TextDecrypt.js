85af2018465e79a8ec5f0b64c89a3d993180d81d
Jo <jolienhoop@gmail.com>
2020-08-03 13:45:52 +0100

    Fix intervals of decrypt animation

--- src/components/Content/TextDecrypt.js
+++ src/components/Content/TextDecrypt.js
@@ -3,19 +3,31 @@ import { useDencrypt } from "use-dencrypt-effect";
 
 const values = ["musician", "developer", "student"];
 
+const decryptOptions = {
+    chars: `a b c d e f g h i j k l m n o p q r s t u v w x y z ! @ # $ % & ( ) 
+          { [ ] } < > ? 0 1 2 3 4 5 6 7 8 9`
+        .replace("\n", "")
+        .split(" "),
+    interval: 70,
+};
+
 export const TextDecrypt = () => {
-    const { result, dencrypt } = useDencrypt();
+    const { result, dencrypt } = useDencrypt(decryptOptions);
 
     React.useEffect(() => {
         let i = 0;
 
-        const action = setInterval(() => {
+        const updateText = () => {
             dencrypt(values[i]);
 
             i = i === values.length - 1 ? 0 : i + 1;
-        }, 4000);
 
-        return () => clearInterval(action);
+            setTimeout(updateText, 4000);
+        };
+
+        const action = setTimeout(updateText, 1000);
+
+        return () => clearTimeout(action);
     }, [dencrypt]);
 
     return <>a {result}</>;
