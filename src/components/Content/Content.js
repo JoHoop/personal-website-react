efeef8b9fc24dd62655a86e7203a605035de2965
Jo <jolienhoop@gmail.com>
2020-08-03 14:20:22 +0100

    Add decrypt effect to name

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Typography, Container } from "@material-ui/core";
 import { IconsRow } from "./IconsRow";
-import { TextDecrypt } from "./TextDecrypt";
+import { TextDecrypt, TextDecryptOnce } from "./TextDecrypt";
 import Resume from "../../resume.json";
 import "./CenterTextMobile.css";
 import Typical from "react-typical";
@@ -14,27 +14,11 @@ export const Content = (props) => {
                     {Resume.basics.x_title}
                 </Typography>
                 <Typography variant="h2" component="h1" gutterBottom>
-                    <Typical
-                        steps={[`${Resume.basics.name}`, 4000]}
-                        loop={Infinity}
-                    />
+                    <TextDecryptOnce />
                 </Typography>
                 <Typography variant="h5" component="h2" gutterBottom>
-                    <Typical
-                        steps={[
-                            "a student",
-                            3000,
-                            "a musician",
-                            3000,
-                            "a developer",
-                            3000,
-                        ]}
-                        loop={Infinity}
-                    />
-                </Typography>
-                {/* <Typography variant="h5" component="h2" gutterBottom>
                     <TextDecrypt />
-                </Typography> */}
+                </Typography>
                 {/* <Typography variant="h5" component="h2" gutterBottom>
                     {Resume.basics.summary}
                 </Typography> */}
--- src/components/Content/TextDecrypt.js
+++ src/components/Content/TextDecrypt.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useDencrypt } from "use-dencrypt-effect";
 
-const values = ["musician", "developer", "student"];
+const values = ["student", "musician", "developer"];
+const name = "Jo Lienhoop";
 
 const decryptOptions = {
     chars: `a b c d e f g h i j k l m n o p q r s t u v w x y z ! @ # $ % & ( ) 
@@ -22,13 +23,29 @@ export const TextDecrypt = () => {
 
             i = i === values.length - 1 ? 0 : i + 1;
 
-            setTimeout(updateText, 4000);
+            setTimeout(updateText, 4500);
         };
 
-        const action = setTimeout(updateText, 1000);
+        const action = setTimeout(updateText, 0);
 
         return () => clearTimeout(action);
     }, [dencrypt]);
 
     return <>a {result}</>;
+};
+
+export const TextDecryptOnce = () => {
+    const { result, dencrypt } = useDencrypt();
+
+    React.useEffect(() => {
+        const updateText = () => {
+            dencrypt(name);
+        };
+
+        const action = setTimeout(updateText, 0);
+
+        return () => clearTimeout(action);
+    }, [dencrypt]);
+
+    return <>{result}</>;
 };
