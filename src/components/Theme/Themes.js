14b18d8dbe74f634ab1beb39e36db7e5e6a57a10
Jo <jolienhoop@gmail.com>
2020-08-10 19:34:36 +0200

    Make font sizes responsive

--- src/components/Theme/Themes.js
+++ src/components/Theme/Themes.js
@@ -1,58 +1,62 @@
-import { createMuiTheme } from "@material-ui/core/styles";
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core";
 
 export const primary = "#17b978";
 export const black = "#171c28";
 export const white = "#fafafa";
 
-export const LightTheme = createMuiTheme({
-    palette: {
-        type: "light",
-        primary: {
-            main: primary,
+export const LightTheme = responsiveFontSizes(
+    createMuiTheme({
+        palette: {
+            type: "light",
+            primary: {
+                main: primary,
+            },
+            secondary: {
+                main: primary,
+            },
         },
-        secondary: {
-            main: primary,
+        typography: {
+            fontSize: 16,
         },
-    },
-    typography: {
-        fontSize: 16,
-    },
-    overrides: {
-        MuiCssBaseline: {
-            "@global": {
-                body: {
-                    color: black,
-                    backgroundColor: white,
+        overrides: {
+            MuiCssBaseline: {
+                "@global": {
+                    body: {
+                        color: black,
+                        backgroundColor: white,
+                    },
                 },
             },
         },
-    },
-});
+    })
+);
 
-export const DarkTheme = createMuiTheme({
-    palette: {
-        type: "dark",
-        primary: {
-            main: primary,
+export const DarkTheme = responsiveFontSizes(
+    createMuiTheme({
+        palette: {
+            type: "dark",
+            primary: {
+                main: primary,
+            },
+            secondary: {
+                main: primary,
+            },
+            background: {
+                default: black,
+            },
         },
-        secondary: {
-            main: primary,
+        typography: {
+            fontSize: 16,
         },
-        background: {
-            default: black,
-        },
-    },
-    typography: {
-        fontSize: 16,
-    },
-    overrides: {
-        MuiCssBaseline: {
-            "@global": {
-                body: {
-                    color: white,
-                    backgroundColor: black,
+        overrides: {
+            MuiCssBaseline: {
+                "@global": {
+                    body: {
+                        color: white,
+                        backgroundColor: black,
+                    },
                 },
             },
         },
-    },
-});
+    })
+);
