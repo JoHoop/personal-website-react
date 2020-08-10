21f04ade80361f685ec3130b714755d569a9b53c
Jo <jolienhoop@gmail.com>
2020-08-10 16:53:30 +0200

    Remove unused component

--- src/components/Content/RepoCard.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export const RepoCard = (props) => {
-    const userSlashRepo = `${props.user}/${props.repo}`;
-    return <div className="repo-card" data-repo={userSlashRepo}></div>;
-};
