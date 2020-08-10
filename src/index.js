c4d6d43b397a76d3eb77e12b556f47c51bdb278e
Jo <jolienhoop@gmail.com>
2020-08-10 20:01:11 +0200

    Patch build script and index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { render } from "react-snapshot";
 import { App } from "./App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
