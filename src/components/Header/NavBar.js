72958539962872a77229cad4d9fee556d02c05cc
Jo Lienhoop <jolienhoop@gmail.com>
2020-08-11 16:33:02 +0200

    Add tooltip to navbar icon

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -1,7 +1,9 @@
 import React, { Fragment } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Avatar, Toolbar, Link } from "@material-ui/core";
+import { Avatar, Toolbar, Link, Tooltip } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
 import { ThemeToggle } from "../Theme/ThemeToggle";
+import Zoom from "@material-ui/core/Zoom";
 import { primary, white } from "../Theme/Themes";
 import { Initials } from "../../utils/getName";
 import Resume from "../../resume.json";
@@ -23,6 +25,15 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+const PrimaryTooltip = withStyles((theme) => ({
+    tooltip: {
+        backgroundColor: theme.palette.primary.main,
+        color: "#fafafa",
+        boxShadow: theme.shadows[1],
+        fontSize: 11,
+    },
+}))(Tooltip);
+
 export const NavBar = () => {
     const classes = useStyles();
 
@@ -37,9 +48,15 @@ export const NavBar = () => {
                     noWrap
                     className={classes.toolbarTitle}
                 >
-                    <Avatar className={`${classes.green}  icon-spin`}>
-                        {Initials}
-                    </Avatar>
+                    <PrimaryTooltip
+                        title={Resume.basics.name}
+                        placement="right"
+                        TransitionComponent={Zoom}
+                    >
+                        <Avatar className={`${classes.green}  icon-spin`}>
+                            {Initials}
+                        </Avatar>
+                    </PrimaryTooltip>
                 </Link>
 
                 <nav>
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const PageNotFound = () => {
-    return <div></div>;
+    return <div>Resume...</div>;
 };
