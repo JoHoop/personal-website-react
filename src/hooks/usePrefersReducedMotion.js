37ea3b4b1e3e2e836094fcef6469849c721688f3
Jo Lienhoop <jolienhoop@gmail.com>
2020-08-26 13:15:21 +0200

    Fix window.matchMedia is not a function

--- src/hooks/usePrefersReducedMotion.js
+++ src/hooks/usePrefersReducedMotion.js
@@ -1,26 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 function usePrefersReducedMotion() {
-  const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
-  const [reduceMotion, setReduceMotion] = useState(mediaQuery.matches);
+    if (!window.matchMedia) return false;
 
-  useEffect(() => {
-    const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
-
-    const handleMediaChange = () => {
-      if (mediaQuery.matches) {
-        setReduceMotion(true);
-      }
-    };
-
-    mediaQuery.addListener(handleMediaChange);
-
-    return () => {
-      mediaQuery.removeListener(handleMediaChange);
-    };
-  }, []);
-
-  return reduceMotion;
+    return window.matchMedia("(prefers-reduced-motion: reduce)").matches;
 }
 
 export default usePrefersReducedMotion;
