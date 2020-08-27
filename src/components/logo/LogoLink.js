96450eb99c101878234523d539acdf64c5884f00
Jo Lienhoop <jolienhoop@gmail.com>
2020-08-27 19:55:18 +0200

    Remove unused styling

--- src/components/logo/LogoLink.js
+++ src/components/logo/LogoLink.js
@@ -22,12 +22,6 @@ const useStyles = makeStyles((theme) => ({
             "0px 3px 5px -1px rgba(0,0,0,0.2),0px 6px 10px 0px rgba(0,0,0,0.14),0px 1px 18px 0px rgba(0,0,0,0.12)",
         borderRadius: "50%",
     },
-    tooltip: {
-        backgroundColor: theme.palette.primary.main,
-        color: "#fafafa",
-        boxShadow: theme.shadows[1],
-        fontSize: 11,
-    },
 }));
 
 export const LogoLink = () => {
