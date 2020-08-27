a61448bc7cfa250060cded8a6b45ccae4d50a4f9
Jo Lienhoop <jolienhoop@gmail.com>
2020-08-28 01:01:58 +0200

    Set log message color from theme

--- src/utils/logCredits.js
+++ src/utils/logCredits.js
@@ -1,8 +1,10 @@
+import { primary } from "../components/theme/Themes";
+
 export const logCredits = () => {
     const pieceEmoji = String.fromCodePoint(0x270c);
 
     const logStyle = [
-        "color: #00bfbf",
+        `color: ${primary}`,
         "font-size: 3em",
         "font-weight: 300",
         "padding: 100px 0px 100px 0px",
