934703dc9184d0ac4552f346c5aa772bed0932ea
Jo Lienhoop <jolienhoop@gmail.com>
2020-08-27 18:14:50 +0200

    Add box shadow to logo

--- src/components/logo/LogoLink.js
+++ src/components/logo/LogoLink.js
@@ -18,6 +18,9 @@ const useStyles = makeStyles((theme) => ({
         position: "absolute",
         top: theme.spacing(6),
         left: theme.spacing(6),
+        boxShadow:
+            "0px 3px 5px -1px rgba(0,0,0,0.2),0px 6px 10px 0px rgba(0,0,0,0.14),0px 1px 18px 0px rgba(0,0,0,0.12)",
+        borderRadius: "50%",
     },
     tooltip: {
         backgroundColor: theme.palette.primary.main,
