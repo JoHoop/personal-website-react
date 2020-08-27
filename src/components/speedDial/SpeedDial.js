d60dbbb6f2900f335e054cfae1b8f74a4ff44a99
Jo Lienhoop <jolienhoop@gmail.com>
2020-08-27 18:58:05 +0200

    Remove unused imports

--- src/components/speedDial/SpeedDial.js
+++ src/components/speedDial/SpeedDial.js
@@ -1,16 +1,10 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Hidden, Link, Backdrop } from "@material-ui/core";
+import { Link, Backdrop } from "@material-ui/core";
 import SpeedDial from "@material-ui/lab/SpeedDial";
 import SpeedDialIcon from "@material-ui/lab/SpeedDialIcon";
 import SpeedDialAction from "@material-ui/lab/SpeedDialAction";
-import FileCopyIcon from "@material-ui/icons/FileCopyOutlined";
-import SaveIcon from "@material-ui/icons/Save";
-import PrintIcon from "@material-ui/icons/Print";
-import ShareIcon from "@material-ui/icons/Share";
-import FavoriteIcon from "@material-ui/icons/Favorite";
 import Resume from "../../resume.json";
-import { primary } from "../theme/Themes";
 
 const useStyles = makeStyles((theme) => ({
     speedDial: {
@@ -20,22 +14,9 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const actions = [
-    { icon: <FileCopyIcon />, name: "Copy" },
-    { icon: <SaveIcon />, name: "Save" },
-    { icon: <PrintIcon />, name: "Print" },
-    { icon: <ShareIcon />, name: "Share" },
-    { icon: <FavoriteIcon />, name: "Like" },
-];
-
 export const SpeedDials = () => {
     const classes = useStyles();
     const [open, setOpen] = React.useState(false);
-    const [hidden, setHidden] = React.useState(false);
-
-    const handleHiddenChange = () => {
-        setHidden((prevHidden) => !prevHidden);
-    };
 
     const handleClose = () => {
         setOpen(false);
@@ -50,7 +31,6 @@ export const SpeedDials = () => {
             key={action.network.toLowerCase()}
             icon={<i className={`${action.x_icon}`}></i>}
             tooltipTitle={action.network}
-            TooltipClasses={classes.tooltip}
             onClick={handleClose}
         >
             <Link
@@ -70,7 +50,7 @@ export const SpeedDials = () => {
             <SpeedDial
                 ariaLabel="SpeedDial"
                 className={classes.speedDial}
-                hidden={hidden}
+                hidden={false}
                 icon={<SpeedDialIcon />}
                 onClose={handleClose}
                 onOpen={handleOpen}
