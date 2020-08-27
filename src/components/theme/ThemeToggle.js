77f4c90f1b00dd7cf319afeb5a0ba219de9f1cdd
Jo Lienhoop <jolienhoop@gmail.com>
2020-08-27 19:43:41 +0200

    Remove unused switch component

--- src/components/theme/ThemeToggle.js
+++ src/components/theme/ThemeToggle.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "./ThemeProvider";
-import { Grid, Switch, Tooltip, IconButton, Zoom } from "@material-ui/core";
+import { Tooltip, IconButton, Zoom } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Brightness4Icon from "@material-ui/icons/Brightness4";
 import Brightness7Icon from "@material-ui/icons/Brightness7";
@@ -23,40 +23,23 @@ export const ThemeToggle = () => {
     const classes = useStyles();
 
     return (
-        <>
-            {/* <Grid component="label" container alignItems="center">
-                <Grid item>
-                    <i className="fas fa-sun" />
-                </Grid>
-                <Grid item>
-                    <Switch
-                        checked={theme === "dark"}
-                        onChange={toggleTheme}
-                        name="dark mode"
-                    />
-                </Grid>
-                <Grid item>
-                    <i className="fas fa-moon" />
-                </Grid>
-            </Grid> */}
-            <Tooltip
-                title={"Toggle light/dark theme"}
-                placement="right"
-                TransitionComponent={Zoom}
+        <Tooltip
+            title={"Toggle light/dark theme"}
+            placement="right"
+            TransitionComponent={Zoom}
+        >
+            <IconButton
+                color="inherit"
+                onClick={toggleTheme}
+                aria-label={"Toggle light/dark theme"}
+                className={classes.iconButton}
             >
-                <IconButton
-                    color="inherit"
-                    onClick={toggleTheme}
-                    aria-label={"Toggle light/dark theme"}
-                    className={classes.iconButton}
-                >
-                    {theme === "light" ? (
-                        <Brightness4Icon className={classes.icon} />
-                    ) : (
-                        <Brightness7Icon className={classes.icon} />
-                    )}
-                </IconButton>
-            </Tooltip>
-        </>
+                {theme === "light" ? (
+                    <Brightness4Icon className={classes.icon} />
+                ) : (
+                    <Brightness7Icon className={classes.icon} />
+                )}
+            </IconButton>
+        </Tooltip>
     );
 };
