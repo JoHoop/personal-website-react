2c247f280cbb190f2db35cd5997c88b2d03d3c8a
Jo Lienhoop <jolienhoop@gmail.com>
2020-09-05 18:01:19 +0200

    Add time of day

--- src/components/content/Today.js
+++ src/components/content/Today.js
@@ -1,22 +1,30 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
 
-export const Today = (props) => {
+export const Today = () => {
     var date = new Date();
+    var hour = date.getHours();
+    var time = `${
+        (hour < 4 && "night") ||
+        (hour < 12 && "morning") ||
+        (hour < 18 && "afternoon") ||
+        (hour < 22 && "evening") ||
+        "night"
+    }`;
     var days = [
-        "Sunday",
+        "weekend",
         "Monday",
         "Tuesday",
         "Wednesday",
         "Thursday",
         "Friday",
-        "Saturday",
+        "weekend",
     ];
     var day = days[date.getDay()];
 
     return (
         <Typography variant="h5" component="h2" gutterBottom>
-            Hope you're having a good {day}
+            Have a good {day === "weekend" ? day : time}.
         </Typography>
     );
 };
