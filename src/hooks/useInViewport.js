b52b9ef4ec8c73a082081ab51fd20ea1f8cf4d72
Jo Lienhoop <jolienhoop@gmail.com>
2020-09-05 16:13:48 +0200

    Convert function to arrow component

--- src/hooks/useInViewport.js
+++ src/hooks/useInViewport.js
@@ -1,31 +1,33 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-function useInViewport(elementRef, unobserveOnIntersect, options = {}) {
-  const [intersect, setIntersect] = useState(false);
-  const [isUnobserved, setIsUnobserved] = useState(false);
+export const useInViewport = (
+    elementRef,
+    unobserveOnIntersect,
+    options = {}
+) => {
+    const [intersect, setIntersect] = useState(false);
+    const [isUnobserved, setIsUnobserved] = useState(false);
 
-  useEffect(() => {
-    if (!elementRef?.current) return;
+    useEffect(() => {
+        if (!elementRef?.current) return;
 
-    const observer = new IntersectionObserver(([entry]) => {
-      const { isIntersecting, target } = entry;
+        const observer = new IntersectionObserver(([entry]) => {
+            const { isIntersecting, target } = entry;
 
-      setIntersect(isIntersecting);
+            setIntersect(isIntersecting);
 
-      if (isIntersecting && unobserveOnIntersect) {
-        observer.unobserve(target);
-        setIsUnobserved(true);
-      }
-    }, options);
+            if (isIntersecting && unobserveOnIntersect) {
+                observer.unobserve(target);
+                setIsUnobserved(true);
+            }
+        }, options);
 
-    if (!isUnobserved) {
-      observer.observe(elementRef.current);
-    }
+        if (!isUnobserved) {
+            observer.observe(elementRef.current);
+        }
 
-    return () => observer.disconnect();
-  }, [elementRef, unobserveOnIntersect, options, isUnobserved]);
+        return () => observer.disconnect();
+    }, [elementRef, unobserveOnIntersect, options, isUnobserved]);
 
-  return intersect;
-}
-
-export default useInViewport;
+    return intersect;
+};
--- src/hooks/usePrefersReducedMotion.js
+++ src/hooks/usePrefersReducedMotion.js
@@ -1,7 +1,5 @@
-function usePrefersReducedMotion() {
+export const usePrefersReducedMotion = () => {
     if (!window.matchMedia) return false;
 
     return window.matchMedia("(prefers-reduced-motion: reduce)").matches;
-}
-
-export default usePrefersReducedMotion;
+};
