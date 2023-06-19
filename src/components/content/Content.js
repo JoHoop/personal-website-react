97e97c84d82c6b80eca834080eb4d10ae20bea03
Eren GUN <erengun4@gmail.com>
2023-06-19 20:06:17 +0300

    remove country from content

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -25,7 +25,6 @@ export const Content = () => {
             </Typography>
             <Typography variant="h5" component="h2" gutterBottom>
                 <TextDecrypt text={`a ${Resume.basics.job}`} />
-                <TextDecrypt text={`from ${Resume.basics.location.country}`} />
             </Typography>
         </Container>
     );
